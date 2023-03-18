54390b8bfc1e51d647ffccdb10282c9b618260b9
roflanmen <bohdan7nava@gmail.com>
2023-03-18 15:12:11 +0200

    Fix heroku deploy

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    app.run(host="localhost", port = os.environ.get('PORT', 5000), debug=True)
+    app.run(host="localhost", port = os.environ.get('PORT', 5000))
